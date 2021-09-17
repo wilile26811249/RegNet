4a110fc837dce16f363657e79673d4ea48f3617b
wilile26811249 <wilile26811249@gmail.com>
2021-09-17 13:06:49 +0800

    Update training pipeline for imagenet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import utils
+import shutil
 import wandb
 from tqdm import tqdm
 from models import create_regnet
@@ -8,18 +9,32 @@ from models import create_regnet
 import torch
 import torch.nn as nn
 from torchvision import transforms as T
+import torchvision.datasets as datasets
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
 
 
 def train_epoch(epoch, net, train_loader, val_loader , criterion, optimizer, scheduler, device):
     """
     Training logic for an epoch
     """
+    global best_acc1
     train_loss = utils.AverageMeter("Epoch losses", ":.4e")
     train_acc1 = utils.AverageMeter("Train Acc@1", ":6.2f")
     train_acc5 = utils.AverageMeter("Train Acc@5", ":6.2f")
+    progress_train = utils.ProgressMeter(
+        num_batches = len(val_loader),
+        meters = [train_loss, train_acc1, train_acc5],
+        prefix = 'Epoch: {} '.format(epoch + 1),
+        batch_info = " Iter"
+    )
     net.train()
 
-    for _, (inputs, targets) in enumerate(tqdm(train_loader)):
+    for it, (inputs, targets) in enumerate(tqdm(train_loader)):
         inputs = inputs.to(device)
         targets = targets.to(device)
 
@@ -31,19 +46,27 @@ def train_epoch(epoch, net, train_loader, val_loader , criterion, optimizer, sch
         train_loss.update(loss.item(), inputs.size(0))
         train_acc1.update(acc1.item(), inputs.size(0))
         train_acc5.update(acc5.item(), inputs.size(0))
+        if it % args.print_freq == 0:
+            progress_train.display(it)
 
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    scheduler.step()
 
+        # Log on Wandb
+        wandb.log({
+            "Loss/train" : train_loss.avg,
+            "Acc@1/train" : train_acc1.avg,
+            "Acc@5/train" : train_acc5.avg,
+        })
+    scheduler.step()
 
     # Validation model
     val_loss = utils.AverageMeter("Val losses", ":.4e")
     val_acc1 = utils.AverageMeter("Val Acc@1", ":6.2f")
     val_acc5 = utils.AverageMeter("Val Acc@5", ":6.2f")
-    progress = utils.ProgressMeter(
+    progress_val = utils.ProgressMeter(
         num_batches = len(val_loader),
         meters = [val_loss, val_acc1, val_acc5],
         prefix = 'Epoch: {} '.format(epoch + 1),
@@ -63,22 +86,34 @@ def train_epoch(epoch, net, train_loader, val_loader , criterion, optimizer, sch
         val_loss.update(loss.item(), inputs.size(0))
         val_acc1.update(acc1.item(), inputs.size(0))
         val_acc5.update(acc5.item(), inputs.size(0))
-        progress.display(it)
+        acc1 = val_acc1.avg
 
-    wandb.log({
-        "Loss/train" : train_loss.avg,
-        "Loss/val" : val_loss.avg,
-        "Acc@1/train" : train_acc1.avg,
-        "Acc@1/val" : val_acc1.avg,
-        "Acc@5/train" : train_acc5.avg,
-        "Acc@5/val" : val_acc5.avg
-    })
+        if it % args.print_freq == 0:
+            progress_val.display(it)
 
+        # Log on Wandb
+        wandb.log({
+            "Loss/val" : val_loss.avg,
+            "Acc@1/val" : val_acc1.avg,
+            "Acc@5/val" : val_acc5.avg
+        })
+
+    is_best = acc1 > best_acc1
+    best_acc1 = max(acc1, best_acc1)
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'state_dict': net.state_dict(),
+        'best_acc1': best_acc1,
+        'optimizer' : optimizer.state_dict(),
+    }, is_best)
     return val_loss.avg, val_acc1.avg, val_acc5.avg
 
 
 if __name__ == "__main__":
+    best_acc1 = 0.0
     parser = argparse.ArgumentParser(description = "Train classification of CMT model")
+    parser.add_argument('--data', metavar = 'DIR', default = './data',
+                help = 'path to dataset')
     parser.add_argument("--gpu_device", type = int, default = 0,
                 help = "Select specific GPU to run the model")
     parser.add_argument('--batch-size', type = int, default = 64, metavar = 'N',
@@ -91,6 +126,8 @@ if __name__ == "__main__":
                 help = 'Learning rate (default: 6e-5)')
     parser.add_argument('--weight-decay', type = float, default = 1e-5, metavar = 'WD',
                 help = 'Weight decay (default: 1e-5)')
+    parser.add_argument('-p', '--print-freq', default = 10, type = int,
+                        metavar='N', help='print frequency (default: 10)')
     parser.add_argument('--model-path', type = str, default = 'weights/model.pth', metavar = 'PATH',
                 help = 'Path to save the model')
     args = parser.parse_args()
@@ -104,30 +141,35 @@ if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu_device)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Get Cifar10 Dataloader
-    kwargs = {
-        'batch_size': args.batch_size,
-        'num_workers': 4,
-    }
-    train_transform = T.Compose([
-        T.RandomCrop(32, padding = 4),
-        T.RandomHorizontalFlip(),
-        T.RandomRotation(10),
-        T.Resize(224),
-        T.ToTensor(),
-        T.Normalize(mean = [0.491, 0.482, 0.447], std = [0.247, 0.243, 0.262])
-    ])
-    test_transform = T.Compose([
-        T.Resize(224),
-        T.ToTensor(),
-        T.Normalize(mean = [0.491, 0.482, 0.447], std = [0.247, 0.243, 0.262])
-    ])
+    # Data loading
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
+    normalize = T.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
 
-    train_loader, valid_laoder, test_loader = utils.get_dataloader(
-        train_transform,
-        test_transform,
-        img_size = 224,
-        **kwargs
+    train_dataset = datasets.ImageFolder(
+        traindir,
+        T.Compose([
+            T.RandomResizedCrop(224),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            normalize,
+    ]))
+    val_dataset = datasets.ImageFolder(
+        valdir,
+        T.Compose([
+            T.Resize(256),
+            T.CenterCrop(224),
+            T.ToTensor(),
+            normalize,
+    ]))
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size = args.batch_size, shuffle = True,
+        num_workers = 4,  pin_memory = True
+    )
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size = args.batch_size, shuffle = False,
+        num_workers = 4, pin_memory = True
     )
 
     # Create model
@@ -136,10 +178,11 @@ if __name__ == "__main__":
 
     # Set loss function and optimizer
     # criterion = nn.CrossEntropyLoss()
-    criterion = nn.functional.cross_entropy
-    optimizer = torch.optim.AdamW(net.parameters(), lr = args.lr,
-        weight_decay = args.weight_decay
-    )
+    criterion = nn.CrossEntropyLoss().cuda(args.gpu_device)
+
+    optimizer = torch.optim.SGD(net.parameters(), args.lr,
+                                momentum = 0.9,
+                                weight_decay = args.weight_decay)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs)
 
     # Using wandb for logging
@@ -150,7 +193,7 @@ if __name__ == "__main__":
     # Train the model
     for epoch in tqdm(range(args.epochs)):
         loss, acc1, acc5 = train_epoch(epoch, net, train_loader,
-            valid_laoder, criterion, optimizer, scheduler, device
+            val_loader, criterion, optimizer, scheduler, device
         )
         print(f"Epoch {epoch} ->  Acc@1: {acc1}, Acc@5: {acc5}")
         # Save model
